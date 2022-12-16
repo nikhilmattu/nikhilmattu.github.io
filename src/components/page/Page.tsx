5d11cb6657d0f2cfd93455d219fdd948792a34c2
Nikhil Mattu <nikhilmattu@gmail.com>
2022-12-15 21:47:03 -0500

    test

---
+++ src/components/page/Page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+
+type Props = {
+  children?: JSX.Element;
+};
+
+const Page: React.FC<Props> = ({ children }) => {
+  // const location = useLocation();
+  // console.log(location)
+
+  return (
+    <div className="Page">
+      <NavBar />
+      {children}
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const HomeComponent: React.FC<Props> = () => {
+  return <div>home</div>;
+};
+
+export default HomeComponent;
