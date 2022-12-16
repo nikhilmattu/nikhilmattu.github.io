81fe2a4334bd6128bc0823f7c8a1022ec176a111
Nikhil Mattu <nikhilmattu@gmail.com>
2022-12-15 21:24:04 -0500

    husky test

--- portfolio/src/routes/Home.tsx
+++ portfolio/src/routes/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react"
-import Page from "../components/page/Page"
 
 type Props = {}
 
 const HomeComponent: React.FC<Props> = () => {
-    return <div>home</div>
+    return <div>
+        
+        
+        home</div>
 }
 
 export default HomeComponent
