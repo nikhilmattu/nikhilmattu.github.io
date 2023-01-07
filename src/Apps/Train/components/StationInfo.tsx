0c3f4a7dac255890ec46feb4ca60e025e501619b
Nikhil Mattu <nikhilmattu@gmail.com>
2023-01-07 15:42:10 -0500

    test

--- src/Apps/Train/components/StationInfo.tsx
+++ src/Apps/Train/components/StationInfo.tsx
@@ -1,20 +1,48 @@
 import React, { useEffect } from "react";
+// const https = require("https");
+// https.get(
+//   "<Feed URI>",
+//   { headers: { "x-api-key": '<Api Key>'}
+//   },
+//   (resp) => {
+//     resp.on('data', (chunk) => {
+//       console.log("Receiving Data");
+//     });
+//     resp.on('end', () => {
+//       console.log("Finished receiving data");
+//     });
+//   }).on("error", (err) => {
+//     console.log("Error: " + err.message);
+//   });
 
 type Props = {
   id: string;
 };
 
-// hnC289tRfc5SWA02qSwxs3ALv4tBPXXr4CQ8Ogzl
+// https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace
+const reqURL = `https://api.wheresthefuckingtrain.com/by-id/82bd`;
+const HEADERS = {
+  "x-api-key": `hnC289tRfc5SWA02qSwxs3ALv4tBPXXr4CQ8Ogzl`,
+  MTA_KEY: `hnC289tRfc5SWA02qSwxs3ALv4tBPXXr4CQ8Ogzl`,
+};
 const StationInfo: React.FC<Props> = ({ id }) => {
   useEffect(() => {
     if (id) {
-      fetch(`https://api.wheresthefuckingtrain.com/by-id/${id}`).then((resp) =>
-        resp.json().then((train) => {
-          console.log(train);
+      fetch(reqURL, {
+        mode: "no-cors",
+        headers: {
+          ...HEADERS,
+        },
+      })
+        .then((resp) => {
+          console.log(resp.body);
+          resp.json().then((train) => {
+            console.log(train);
+          });
         })
-      ).catch(err => {
-        console.log(err)
-      });
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }, [id]);
   return <div>station: {id}</div>;
